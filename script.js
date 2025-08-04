2a2a75048dd9b1ecec00ed7e86904644b82416e9
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-08-04 09:27:25 +0000

    Loop Control

--- script.js
+++ script.js
@@ -119,6 +119,7 @@ document.addEventListener("DOMContentLoaded", () => {
             for_loops: 'snippets/bash/for_loops.txt',
             while_loops: 'snippets/bash/while_loops.txt',
             until_loops: 'snippets/bash/until_loops.txt',
+            loop_control: 'snippets/bash/break_continue.txt',
             
         },
         c: {
