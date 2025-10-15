e5f9009bcb06793a5792b89b6e5bdcf29f5c8139
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-10-15 21:21:29 +0000

    Dart Streams Basics JS

--- script.js
+++ script.js
@@ -224,6 +224,7 @@ document.addEventListener("DOMContentLoaded", () => {
             generics: 'snippets/dart/generics.txt',
             extension_method: 'snippets/dart/extension-methods.txt',
             async_await: 'snippets/dart/async-awaits.txt',
+            streams_basics: 'snippets/dart/streams-basic.txt'
             
         },
         elixir: {
