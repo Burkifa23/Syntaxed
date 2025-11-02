f6c6a368cb396e9300b86a679486299cf4b91a5a
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-02 22:04:29 +0000

    Java Assertions Debugging Tested

--- script.js
+++ script.js
@@ -280,7 +280,8 @@ document.addEventListener("DOMContentLoaded", () => {
             multi_catch_resources: 'snippets/java/multi-catch-resources.txt',
             exception_propagation: 'snippets/java/exception-propagation.txt',
             exception_handling_patterns: 'snippets/java/exception-handling-patterns.txt',
-            
+            assertions_debugging: 'snippets/java/assertions-debugging.txt',
+
 
             
             
